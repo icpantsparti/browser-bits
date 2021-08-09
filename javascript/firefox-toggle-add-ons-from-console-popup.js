ae0ff810a55d52b511be7dad95891536daa06118
icpantsparti <35049679+icpantsparti@users.noreply.github.com>
2021-08-09 18:53:23 +0100

    Create firefox-toggle-add-ons-from-console-popup.js

---
+++ javascript/firefox-toggle-add-ons-from-console-popup.js
@@ -0,0 +1,9 @@
+// JavaScript for Mozilla Firefox console
+// Firefox: Toggle Add-ons between enabled/disabled (single or bulk) popup
+//
+// Name         : firefox-toggle-add-ons-from-console-popup.js
+// Project      : https://github.com/icpantsparti/browser-bits
+// Version      :
+// File/Update  : https://raw.githubusercontent.com/icpantsparti/browser-bits/main/firefox-toggle-add-ons-from-console-popup.js
+// License (MIT): https://raw.githubusercontent.com/icpantsparti/browser-bits/main/LICENSE
+// Disclaimer   : Use with care at your own risk, and verify any results
