d629f99ec6dedb4d038948401b4005c516f30249
icpantsparti <35049679+icpantsparti@users.noreply.github.com>
2021-08-09 18:55:28 +0100

    Update firefox-toggle-add-ons-from-console-popup.js

--- javascript/firefox-toggle-add-ons-from-console-popup.js
+++ javascript/firefox-toggle-add-ons-from-console-popup.js
@@ -4,6 +4,6 @@
 // Name         : firefox-toggle-add-ons-from-console-popup.js
 // Project      : https://github.com/icpantsparti/browser-bits
 // Version      :
-// File/Update  : https://raw.githubusercontent.com/icpantsparti/browser-bits/main/firefox-toggle-add-ons-from-console-popup.js
+// File/Update  : https://raw.githubusercontent.com/icpantsparti/browser-bits/main/javascript/firefox-toggle-add-ons-from-console-popup.js
 // License (MIT): https://raw.githubusercontent.com/icpantsparti/browser-bits/main/LICENSE
 // Disclaimer   : Use with care at your own risk, and verify any results
