df882c466385d542479fd063676fb4ef9df38f73
icpantsparti <35049679+icpantsparti@users.noreply.github.com>
2021-09-02 14:31:46 +0000

    v2021.09.02

---
+++ firefox-style/chrome_debugger_profile/user.js
@@ -0,0 +1,10 @@
+/*******************************************************************************
+** [FIREFOX-PROFILE-FOLDER]/chrome_debugger_profile/user.js
+********************************************************************************
+** File/Update  : https://raw.githubusercontent.com/icpantsparti/browser-bits/main/firefox-style/chrome_debugger_profile/user.js
+** Version      : 2021.09.02
+** License (MIT): https://raw.githubusercontent.com/icpantsparti/browser-bits/main/LICENSE
+** Project      : https://github.com/icpantsparti/browser-bits
+** Disclaimer   : Use with care at your own risk, and verify any results
+*******************************************************************************/
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
